3ddd55a7cf39d2aee2c2ff52be8ac8cf36f919f5
Sumon Mitra <sumonmitra.21@gmail.com>
2025-01-30 20:51:41 +0530

    init

---
+++ FE-prac/form-builder/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ return (
+    <div className='h-screen bg-gray-700' >
+      <h1 className='text-2xl text-white text-center'>Sumon Mitra</h1>
+    </div>
+  )
+}
+
+export default App
+ 
